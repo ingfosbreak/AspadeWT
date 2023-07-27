c9afc73238d01b802fbf331c181070849bce2f11
ingfosbreak <ingfosbreak@outlook.com>
2023-07-27 20:22:57 +0700

    Add: app-layout and profile setting

---
+++ laravel/resources/views/components/profileClick.blade.php
@@ -0,0 +1,52 @@
+<div class="none" id="profile">
+  <div class="contents">
+    <h1>Contents</h1>
+  </div>
+</div>
+
+
+
+
+
+
+<style>
+
+.none {
+    display:none;
+}
+.cover{
+    background:rgba(0,0,0,0);
+    position:fixed;
+    width:300px;
+    height:300px;
+    display:flex;
+    flex-direction:column;
+    
+    
+}
+.contents{
+    width:300px;
+    height:300px;
+    background:#f1f1f1;
+    border:2px solid #ccc;
+    border-radius:30px;
+    display:flex;
+    justify-content:center;
+    text-align:center;
+    
+}
+
+.close{
+    border:2px solid #ccc;
+    border-radius:15px;
+    padding:5px 15px;
+    position:absolute;
+    bottom:15px;
+    text-align:center;
+    left:30%;
+    width:100px;
+    box-shadow:inset 0px 0px 10px 5px #ccc;
+}
+</style>
+
+
