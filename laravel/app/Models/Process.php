ff549dc44532f6da1d8522ac3e0be382d408892d
ingfosbreak <ingfosbreak@outlook.com>
2023-07-29 06:50:40 +0700

    Fix: Process read bug

--- laravel/app/Models/Process.php
+++ laravel/app/Models/Process.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use App\Models\UserEntry;
 use App\Models\Event;
 
