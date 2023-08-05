bcb81f8054e04f1a32600594ec3a25e7a8a058d4
ingfosbreak <ingfosbreak@outlook.com>
2023-08-05 21:08:17 +0700

    Fix: bug fixed navbar

--- laravel/resources/views/layouts/navbar.blade.php
+++ laravel/resources/views/layouts/navbar.blade.php
@@ -3,8 +3,8 @@
 <div class="flex flex-wrap">
     <section class="relative mx-auto">
         <!-- navbar -->
-        <nav class="flex justify-between bg-gray-900 text-white w-screen">
-            <div class="px-5 xl:px-12 py-6 flex w-full items-center">
+        <nav class="flex justify-between bg-gray-900 text-white w-screen px-5 xl:px-12 py-6 items-center">
+
                 <a class="text-3xl font-bold font-heading" href="#">
                     <img class="h-16"
                         src="https://media.discordapp.net/attachments/1133336224237629504/1135067839271796806/image.png?width=1368&height=701"
@@ -14,17 +14,18 @@
                 <ul class="hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12">
                     
                 </ul>
-                <div class="flex  rounded bg-white" x-data="{ search: '' }">
+                
+                <div class="flex rounded bg-white" >
                     <input type="search"
-                        class="w-full border-none bg-transparent px-4 py-1 text-gray-900 focus:outline-none"
-                        placeholder="search" x-model="search">
+                        class=" border-none bg-transparent px-4 py-1 text-gray-900 focus:outline-none"
+                        placeholder="search">
                     <button class="m-2 rounded px-4 py-2 font-semibold text-gray-100 bg-gray-500 cursor-not-allowed"
                         :class="(search) ? 'bg-purple-500' : 'bg-gray-500 cursor-not-allowed'" :disabled="!search"
                         disabled="disabled">search</button>
                 </div>
                 <!-- Sign In / Register      -->
                 <div class="flex items-center hover:text-gray-200" id="profilebut">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 hover:text-gray-200" fill="none"
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 hover:text-gray-200" fill="none"
                         viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                             d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
@@ -32,13 +33,8 @@
                 </div>
 
 
-            </div>
-</div>
-
-
-</nav>
-
-</section>
+        </nav>
+    </section>
 </div>
 
 <div class="flex flex-wrap">
