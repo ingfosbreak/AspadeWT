7ba96056b3052c07c46c4705541ae76260f16fb6
ingfosbreak <ingfosbreak@outlook.com>
2023-08-06 13:36:48 +0700

    Update: kanban

--- laravel/resources/views/test.blade.php
+++ laravel/resources/views/test.blade.php
@@ -1,36 +1,191 @@
-@extends('layouts.app')
 
-@section('content')
-    @vite(['resources/css/main.css','resources/js/app.js'])
-    <div class="all">
-        <button id ='but2' class="edit">Edit</button>
-        <div class="parent" id="parent">
-            <div class="thin" id = 'test111'>1</div>
-            <div class="thin">2</div>
-
-            <div class="thin">3</div>
-            <div class="thin">4</div>
-
-        
+    @vite(['resources/css/app.css','resources/css/main.css','resources/js/app.js'])
+    <div class="h-screen p-2">
+  <div class="grid lg:grid-cols-7 md:grid-cols-4 sm:grid-cols-2 gap-5">
+    <!-- To-do -->
+    <div class="bg-white rounded px-2 py-2" >
+      <!-- board category header -->
+      <div class="flex flex-row justify-between items-center mb-2 mx-1">
+        <div class="flex items-center">
+          <h2 class="bg-red-100 text-sm w-max px-1 rounded mr-2 text-gray-700">To-do</h2>
+          <p class="text-gray-400 text-sm">3</p>
+        </div>
+        <div class="flex items-center text-gray-300">
+          <p class="mr-2 text-2xl">---</p>
+          <p class="text-2xl">+</p>
+        </div>
+      </div>
+      <!-- board card -->
+      <div class="grid grid-rows-2 gap-2" id="first">
+        <div class="p-2 rounded shadow-sm border-gray-100 border-2">
+          <h3 class="text-sm mb-3 text-gray-700">Social media</h3>
+          <p class="bg-red-100 text-xs w-max p-1 rounded mr-2 text-gray-700">To-do</p>
+          <div class="flex flex-row items-center mt-2">
+            <div class="bg-gray-300 rounded-full w-4 h-4 mr-3"></div>
+            <a href="#" class="text-xs text-gray-500">Sophie Worso</a>
+          </div>
+          <p class="text-xs text-gray-500 mt-2">2</p>
         </div>
 
-
-        <div class="parent2" id="parent2">
-            <div class="thin2">1</div>
-            <div class="thin2">2</div>
-
-            <div class="thin2">3</div>
-            <div class="thin2">4</div>
-
-        
+        <div class="p-2 rounded shadow-sm border-gray-100 border-2">
+          <h3 class="text-sm mb-3 text-gray-700">Review survey results</h3>
+          <p class="bg-red-100 text-xs w-max p-1 rounded mr-2 text-gray-700">To-do</p>
+          <div class="flex flex-row items-center mt-2">
+            <div class="bg-gray-300 rounded-full w-4 h-4 mr-3"></div>
+            <a href="#" class="text-xs text-gray-500">Sophie Worso</a>
+          </div>
+          <p class="text-xs text-gray-500 mt-2">1</p>
         </div>
+
+        <div class="p-2 rounded shadow-sm border-gray-100 border-2">
+          <h3 class="text-sm mb-3 text-gray-700">Research video marketing</h3>
+          <p class="bg-red-100 text-xs w-max p-1 rounded mr-2 text-gray-700">To-do</p>
+          <div class="flex flex-row items-center mt-2">
+            <div class="bg-gray-300 rounded-full w-4 h-4 mr-3"></div>
+            <a href="#" class="text-xs text-gray-500">Sophie Worso</a>
+          </div>
+          <p class="text-xs text-gray-500 mt-2">3</p>
+        </div>
+      </div>
+      <div class="flex flex-row items-center text-gray-300 mt-2 px-1">
+        <p class="rounded mr-2 text-2xl">+</p>
+        <p class="pt-1 rounded text-sm">New</p>
+      </div>
+    </div>
+
+    <!-- WIP Kanban -->
+    <div class="bg-white rounded px-2 py-2" >
+      <!-- board category header -->
+      <div class="flex flex-row justify-between items-center mb-2 mx-1">
+        <div class="flex items-center">
+          <h2 class="bg-yellow-100 text-sm w-max px-1 rounded mr-2 text-gray-700">WIP</h2>
+          <p class="text-gray-400 text-sm">2</p>
+        </div>
+        <div class="flex items-center text-gray-300">
+          <p class="mr-2 text-2xl">---</p>
+          <p class="text-2xl">+</p>
+        </div>
+      </div>
+      <!-- board card -->
+      <div class="grid grid-rows-2 gap-2" id="second">
+        <div class="p-2 rounded shadow-sm border-gray-100 border-2">
+          <h3 class="text-sm mb-3 text-gray-700">Blog post live</h3>
+          <p class="bg-yellow-100 text-xs w-max p-1 rounded mr-2 text-gray-700">WIP</p>
+          <div class="flex flex-row items-center mt-2">
+            <div class="bg-gray-300 rounded-full w-4 h-4 mr-3"></div>
+            <a href="#" class="text-xs text-gray-500">Sophie Worso</a>
+          </div>
+          <p class="text-xs text-gray-500 mt-2">Jun 21, 2019</p>
+          <p class="text-xs text-gray-500 mt-2">2</p>
+        </div>
+
+        <div class="p-2 rounded shadow-sm border-gray-100 border-2">
+          <h3 class="text-sm mb-3 text-gray-700">Email campaign</h3>
+          <p class="bg-yellow-100 text-xs w-max p-1 rounded mr-2 text-gray-700">WIP</p>
+          <div class="flex flex-row items-center mt-2">
+            <div class="bg-gray-300 rounded-full w-4 h-4 mr-3"></div>
+            <a href="#" class="text-xs text-gray-500">Sophie Worso</a>
+          </div>
+          <p class="text-xs text-gray-500 mt-2">Jun 21, 2019 &#10141; Jun 21, 2019</p>
+          <p class="text-xs text-gray-500 mt-2">1</p>
+        </div>
+      </div>
+      <div class="flex flex-row items-center text-gray-300 mt-2 px-1">
+        <p class="rounded mr-2 text-2xl">+</p>
+        <p class="pt-1 rounded text-sm">New</p>
+      </div>
     </div>
     
+    <!-- Complete Kanban -->
+    <div class="bg-white rounded px-2 py-2">
+      <!-- board category header -->
+      <div class="flex flex-row justify-between items-center mb-2 mx-1">
+        <div class="flex items-center">
+          <h2 class="bg-green-100 text-sm w-max px-1 rounded mr-2 text-gray-700">Complete</h2>
+          <p class="text-gray-400 text-sm">4</p>
+        </div>
+        <div class="flex items-center">
+          <p class="text-gray-300 mr-2 text-2xl">---</p>
+          <p class="text-gray-300 text-2xl">+</p>
+        </div>
+      </div>
+      <!-- board card -->
+      
+      <div class="grid grid-rows-2 gap-2"  id="third">
+        <div class="p-2 rounded shadow-sm border-gray-100 border-2">
+          <h3 class="text-sm mb-3 text-gray-700">Morning emails and to-do list</h3>
+          <p class="bg-green-100 text-xs w-max p-1 rounded mr-2 text-gray-700">Complete</p>
+          <div class="flex flex-row items-center mt-2">
+            <div class="bg-gray-300 rounded-full w-4 h-4 mr-3"></div>
+            <a href="#" class="text-xs text-gray-500">Sophie Worso</a>
+          </div>
+          <p class="text-xs text-gray-500 mt-2">Jun 21, 2019</p>
+          <p class="text-xs text-gray-500 mt-2">1</p>
+        </div>
+
+        <div class="p-2 rounded shadow-sm border-gray-100 border-2">
+          <h3 class="text-sm mb-3 text-gray-700">Blog post</h3>
+          <p class="bg-green-100 text-xs w-max p-1 rounded mr-2 text-gray-700">Complete</p>
+          <div class="flex flex-row items-center mt-2">
+            <div class="bg-gray-300 rounded-full w-4 h-4 mr-3"></div>
+            <a href="#" class="text-xs text-gray-500">Sophie Worso</a>
+          </div>
+          <p class="text-xs text-gray-500 mt-2">Jun 20, 2019</p>
+          <p class="text-xs text-gray-500 mt-2">5</p>
+        </div>
+
+        <div class="p-2 rounded shadow-sm border-gray-100 border-2">
+          <h3 class="text-sm mb-3 text-gray-700">Reconcile accounts</h3>
+          <p class="bg-green-100 text-xs w-max p-1 rounded mr-2 text-gray-700">Complete</p>
+          <div class="flex flex-row items-center mt-2">
+            <div class="bg-gray-300 rounded-full w-4 h-4 mr-3"></div>
+            <a href="#" class="text-xs text-gray-500">Sophie Worso</a>
+          </div>
+          <p class="text-xs text-gray-500 mt-2">Jun 19, 2019</p>
+          <p class="text-xs text-gray-600 mt-2">4</p>
+        </div>
+
+        <div class="p-2 rounded shadow-sm border-gray-100 border-2">
+          <h3 class="text-sm mb-3 text-gray-700">Website AB test</h3>
+          <p class="bg-green-100 text-xs w-max p-1 rounded mr-2 text-gray-700">Complete</p>
+          <div class="flex flex-row items-center mt-2">
+            <div class="bg-gray-300 rounded-full w-4 h-4 mr-3"></div>
+            <a href="#" class="text-xs text-gray-500">Sophie Worso</a>
+          </div>
+          <p class="text-xs text-gray-500 mt-2">Jun 18, 2019</p>
+          <p class="text-xs text-gray-600 mt-2">3</p>
+        </div>
+      </div>
+      <div class="flex flex-row items-center text-gray-300 mt-2 px-1">
+        <p class="rounded mr-2 text-2xl">+</p>
+        <p class="pt-1 rounded text-sm">New</p>
+      </div>
+    </div>
+
+    <div class="bg-white rounded px-2 py-2">
+      <!-- board category header -->
+      <div class="flex flex-row justify-between items-center mb-2 mx-1">
+        <div class="flex items-center">
+          <h2 class="bg-gray-200 w-4 px-1 rounded mr-2 text-sm text-center">.</h2>
+          <p class="text-gray-400 text-sm">0</p>
+        </div>
+        <div class="flex items-center text-gray-300">
+          <p class="mr-2 text-2xl">---</p>
+          <p class="text-2xl">+</p>
+        </div>
+      </div>
+      <div class="flex flex-row items-center text-gray-300 mt-2 px-1">
+        <p class="rounded mr-2 text-2xl">+</p>
+        <p class="pt-1 rounded text-sm">New</p>
+      </div>
+    </div>
+  </div>
+</div>
     <script type="module">
 
         $( document ).ready(function() {
             
-            const drake = dragula([$('#parent').get(0), $('#parent2').get(0)], { revertOnSpill: true });
+            const drake = dragula([$('#first').get(0), $('#second').get(0),$('#third').get(0)], { revertOnSpill: true });
             
             console.log(drake);
             console.log($('#test111').html());
@@ -77,7 +232,7 @@
     
     </script>
     
-@endsection
+
 
 
     
