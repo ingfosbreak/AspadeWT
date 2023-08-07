a7e605d3e0f62b57c8bea5fde1af700f3c391fc9
Dancing squirrel <saran.wongk@ku.th>
2023-08-07 13:39:44 +0700

    Update: form.eventuser

--- laravel/resources/views/event/form.blade.php
+++ laravel/resources/views/event/form.blade.php
@@ -1,7 +1,8 @@
 @vite(['resources/css/app.css', 'resources/js/app.js'])
-<section class="bg-gray-50 dark:bg-gray-900" style="background-color: #40000f;">
+<body class="py-20 bg-slate-900 w-full">
+<section id="testimonies" class="py-20 w-full">
     <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <div class="flex flex-col items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+        <div class="flex flex-col items-center mb-6 text-2xl font-semibold text-gray-900">
             <img class=" h-32 mr-2"
                 src="https://media.discordapp.net/attachments/1133336224237629504/1135067839271796806/image.png?width=1368&height=701"
                 alt="logo">
@@ -22,54 +23,58 @@
                             placeholder="name@company.com" required="">
                     </div>
                     <div>
-                        <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
-                            email</label>
-                        <input type="email" name="email" id="email"
+                        <label for="user" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">userName</label>
+                        <input type="user" name="user" id="user"
                             class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                            placeholder="name@company.com" required="">
+                            placeholder="get username " required="">
+                    </div>
+                    <div>
+                        <label for="stuff" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Count of stuff</label>
+                        <input type="stuff" name="stuff" id="stuff"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="int stuff" required="">
+                    </div>
+                    <div>
+                        <label for="budget" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"> Budget</label>
+                        <input type="budget" name="budget" id="budget"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="cost" required="">
+                    </div>
+                    <div>
+                        <label for="date" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"> Date</label>
+                        <input type="date" name="date" id="date"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="d/m/y" required="">
+                    </div>
+                    <div>
+                        <label for="location" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"> Location</label>
+                        <input type="location" name="location" id="location"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="-" required="">
                     </div>
 
-                    <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
-                        message</label>
-                    <textarea id="message" rows="4"
+                    <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">description</label>
+                    <textarea id="description" rows="4"
                         class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="Write your thoughts here..."></textarea>
+                        placeholder="Write your Description here..."></textarea>
 
                     <div>
                         <label for="password"
                             class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                        <input type="password" name="password" id="password" placeholder="••••••••"
+                        <input type="password" name="password" id="password" placeholder="password"
                             class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             required="">
                         </input>
-                        <div class="flex items-center justify-between">
-                            <div class="flex items-start">
-                                <div class="flex items-center h-5">
-                                    <input id="remember" aria-describedby="remember" type="checkbox"
-                                        class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                                        required="">
-                                </div>
-                                <div class="ml-3 text-sm">
-                                    <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
-                                </div>
-                            </div>
-                            <a href="#"
-                                class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot
-                                password?</a>
-
-
-                        </div>
+                        
                         <button type="submit"
-                            class="w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+                            class="mt-5 w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
                             style="margin-right: 10px; margin-bottom: 10px;">
                             Sign
                         </button>
-                        <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                            Don’t have an account yet? <a href="#"
-                                class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
-                        </p>
+                        
                 </form>
             </div>
         </div>
     </div>
 </section>
+</body>
