a4e1105a20194085f80d154afcf9442f631d56fc
ingfosbreak <ingfosbreak@outlook.com>
2023-08-11 23:24:49 +0700

    Fix: icon profile click

--- laravel/resources/views/layouts/navbar.blade.php
+++ laravel/resources/views/layouts/navbar.blade.php
@@ -25,8 +25,8 @@
             </nav>
     
             <!-- Sign In / Register      -->
-            <div class="flex items-center hover:text-gray-200" id="profilebut">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 hover:text-gray-200" fill="none"
+            <div class="flex items-center " id="profilebut">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 " fill="none"
                     viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                         d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
