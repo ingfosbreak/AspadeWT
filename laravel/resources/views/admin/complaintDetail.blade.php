19046fe541288c73e34469d2aec898ed14c8fde3
Dancing squirrel <saran.wongk@ku.th>
2023-08-22 08:59:32 +0700

    Update :fixed bug

--- laravel/resources/views/admin/complaintDetail.blade.php
+++ laravel/resources/views/admin/complaintDetail.blade.php
@@ -34,25 +34,25 @@
                                 name</label>
                             <input type="string" name="name" id="disable-input"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                                placeholder="{{$event->name}}" value="{{$event->name}}" aria-label="disable input">
+                                placeholder="{{$event->name}}" value="{{$event->name}}" aria-label="disable input" disabled>
                         </div>
                         <div>
                             <label for="num_member" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Num Member</label>
                             <input type="number" name="num_member" id="num_member"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                                placeholder="{{$event->num_member}}" value="{{$event->num_member}}">
+                                placeholder="{{$event->num_member}}" value="{{$event->num_member}}"disabled>
                         </div>
                         <div>
                             <label for="num_staff" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Num Staff</label>
                             <input type="number" name="num_staff" id="num_staff"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                                placeholder="{{$event->num_staff}}" value="{{$event->num_staff}}">
+                                placeholder="{{$event->num_staff}}" value="{{$event->num_staff}}"disabled>
                         </div>
                         <div>
                             <label for="budget" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Budget</label>
                             <input type="number" name="budget" id="budget"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                                placeholder="{{$event->budget}}" value="{{$event->budget}}">
+                                placeholder="{{$event->budget}}" value="{{$event->budget}}"disabled>
                         </div>
                         <!-- Category -->
                         <div>
@@ -60,7 +60,7 @@
                                         class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Activity Type</label>
                                     <select type="text" id="category" name="category"  
                                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                        placeholder="Choose a role for joining" required="">
+                                        placeholder="Choose a role for joining" required="" disabled>
                                         <option value="outdoor">outdoor</option>
                                         <option value="indoor">indoor</option>
                                         <option value="concert">concert</option>
@@ -75,7 +75,7 @@
                         <div class="relative z-0">
                             <input type="date" id="date_register" name="date_register"
                                     class="block py-2.5 px-0 w-full text-base bg-transparent border-0 border-b-2 border-gray-300 appearance-none text-black focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                                    placeholder="d/m/y" autocomplete="off" placeholder="{{$event->date_register}}" value="{{$event->date_register}}"/>
+                                    placeholder="d/m/y" autocomplete="off" placeholder="{{$event->date_register}}" value="{{$event->date_register}}" disabled/>
                             <label for="date_register"
                                     class="absolute text-sm dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
                                     Application Opening Date</label>
@@ -84,7 +84,7 @@
                         <div class="relative z-0">
                             <input type="date" id="date_start" name="date_start"
                                     class="block py-2.5 px-0 w-full text-base bg-transparent border-0 border-b-2 border-gray-300 appearance-none text-black focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                                    placeholder="d/m/y" autocomplete="off" placeholder="{{$event->date_start}}" value="{{$event->date_start}}"/>
+                                    placeholder="d/m/y" autocomplete="off" placeholder="{{$event->date_start}}" value="{{$event->date_start}}" disabled/>
                             <label for="date_start"
                                     class="absolute text-sm dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">                                    
                                     Event Start Date</label>
@@ -93,7 +93,7 @@
                         <div class="relative z-0">
                             <input type="date" id="date_close" name="date_close"
                                     class="block py-2.5 px-0 w-full text-base bg-transparent border-0 border-b-2 border-gray-300 appearance-none text-black focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                                    placeholder="d/m/y" autocomplete="off" placeholder="{{$event->date_close}}" value="{{$event->date_close}}"/>
+                                    placeholder="d/m/y" autocomplete="off" placeholder="{{$event->date_close}}" value="{{$event->date_close}}" disabled/>
                             <label for="date_close"
                                     class="absolute text-sm dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
                                     Closing Date</label>
@@ -102,7 +102,7 @@
                         <div class="relative z-0">
                                     <input type="text" id="location" name="location"
                                         class="block py-2.5 px-0 w-full text-base bg-transparent border-0 border-b-2 border-gray-300 appearance-none  focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                                        placeholder="{{$event->location}}" value="{{$event->location}}" autocomplete="off" />
+                                        placeholder="{{$event->location}}" value="{{$event->location}}" autocomplete="off" disabled/>
                                     <label for="location"
                                         class="absolute text-sm dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
                                         Event Location </label>
@@ -112,11 +112,7 @@
                                     class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">description</label>
                                 <textarea type=text id="description" name="description" rows="4"
                                     class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 "
-                                    placeholder="{{$event->description}}" value="{{$event->description}}"></textarea>
-
-                        <button type="submit"
-                            class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit
-                            edit</button>
+                                    placeholder="{{$event->description}}" value="{{$event->description}}" disabled></textarea>
 
                     </form>
                 </div>
@@ -157,7 +153,7 @@
                     
                     <a href="{{ route('admin.complaintDetail.behide',['event' => $event])}}"
                         class="inline-flex items-center border border-indigo-300 px-3 py-1.5 rounded-md text-indigo-500 hover:bg-indigo-50  my-5">
-                        <span class="ml-1 font-bold text-lg">Detail</span>
+                        <span class="ml-1 font-bold text-lg">Announcement</span>
                     </a>
 
                     <div class=" flex justify-end px-5  gap-3">
